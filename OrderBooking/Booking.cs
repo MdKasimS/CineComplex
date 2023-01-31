c9d3e9ec21c9a143280f4e5f92219f5264e79e3a
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-01-31 20:34:09 +0530

    Unable to remove non visual character.

--- OrderBooking/Booking.cs
+++ OrderBooking/Booking.cs
@@ -1,30 +1,31 @@
+using System;
+
 namespace OrderBooking
 {
-    public class Booking
+public class booking
+{
+    public booking(string sid, string sn, int nos, string seattype, string email, string amount)
     {
-        public Booking(string sid, string sn, int nos, string seatType, string email, string amount)
-        {
-            private string? _customerName;
-            private string? _seatType;
-            private string? _email;
-            private string? _bookingStatus;
-            private int? _bookingId;
-            private int? _ShowId;
-            private int? _numberOfSeats;
-            private List<int>? _seatNumbers;
-            private decimal? _amount;
-            private DateTime? _dateTime;
+        private string? _customername;
+        private string? _seattype;
+        private string? _email;
+        private string? _bookingstatus;
+        private int? _bookingid;
+        private int? _showid;
+        private int? _numberofseats;
+        private List<int>? _seatnumbers;
+        private decimal? _amount;
+        private DateTime? _datetime;
 
-        public string? CustomerName { get => _customerName; set => _customerName = value; }
-        public string? SeatType { get => _seatType; set => _seatType = value; }
-        public string? Email { get => _email; set => _email = value; }
-        public string? BookingStatus { get => _bookingStatus; set => _bookingStatus = value; }
-        public DateTime? DateTime { get => _dateTime; set => _dateTime = value; }
-        public decimal? Amount { get => _amount; set => _amount = value; }
-        public int? NumberOfSeats { get => _numberOfSeats; set => _numberOfSeats = value; }
-        public int? BookingId { get => _bookingId; set => _bookingId = value; }
-        public int? ShowId { get => _ShowId; set => _ShowId = value; }
-    }
-    }
+    public string? Customername { get => _customername; set => _customername = value; }
+    public string? Seattype { get => _seattype; set => _seattype = value; }
+    public string? Email { get => _email; set => _email = value; }
+    public string? Bookingstatus { get => _bookingstatus; set => _bookingstatus = value; }
+    public DateTime? Datetime { get => _datetime; set => _datetime = value; }
+    public decimal? Amount { get => _amount; set => _amount = value; }
+    public int? Numberofseats { get => _numberofseats; set => _numberofseats = value; }
+    public int? Bookingid { get => _bookingid; set => _bookingid = value; }
+    public int? Showid { get => _showid; set => _showid = value; }
 }
-    
+}
+}
