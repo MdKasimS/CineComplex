dd9ecf8809362fbcb9f87d7272bd46f70a3bf655
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-02-01 12:58:05 +0530

    Booking.cs was finalised with all errors solved.

--- OrderBooking/Booking.cs
+++ OrderBooking/Booking.cs
@@ -1,31 +1,36 @@
-using System;
-
 namespace OrderBooking
 {
-public class booking
-{
-    public booking(string sid, string sn, int nos, string seattype, string email, string amount)
+    public class Booking
     {
-        private string? _customername;
-        private string? _seattype;
         private string? _email;
-        private string? _bookingstatus;
-        private int? _bookingid;
-        private int? _showid;
-        private int? _numberofseats;
-        private List<int>? _seatnumbers;
-        private decimal? _amount;
-        private DateTime? _datetime;
+        private string? _seatType;
+        private string? _customerName;
+        private string? _bookingStatus;
+        private int _bookingId;
+        private int _showId;
+        private int _numberOfSeats;
+        private List<int>? _seatNumbers;
+        private DateTime _dateTime;
+        private decimal _amount;
 
-    public string? Customername { get => _customername; set => _customername = value; }
-    public string? Seattype { get => _seattype; set => _seattype = value; }
-    public string? Email { get => _email; set => _email = value; }
-    public string? Bookingstatus { get => _bookingstatus; set => _bookingstatus = value; }
-    public DateTime? Datetime { get => _datetime; set => _datetime = value; }
-    public decimal? Amount { get => _amount; set => _amount = value; }
-    public int? Numberofseats { get => _numberofseats; set => _numberofseats = value; }
-    public int? Bookingid { get => _bookingid; set => _bookingid = value; }
-    public int? Showid { get => _showid; set => _showid = value; }
-}
-}
+        public string? Email { get => _email; set => _email = value; }
+        public string? BookingStatus { get => BookingStatus1; set => BookingStatus1 = value; }
+        public int ShowId { get => ShowId1; set => ShowId1 = value; }
+
+        public List<int>? GetSeatNumbers1()
+        { return _seatNumbers; }
+        public void SetSeatNumbers1(List<int>? value)
+        {
+            _seatNumbers = value;
+        }
+
+        public string? SeatType { get => _seatType; set => _seatType = value; }
+        public DateTime DateTime { get => _dateTime; set => _dateTime = value; }
+        public decimal Amount { get => _amount; set => _amount = value; }
+        public int ShowId1 { get => _showId; set => _showId = value; }
+        public int NumberOfSeats { get => _numberOfSeats; set => _numberOfSeats = value; }
+        public string? BookingStatus1 { get => _bookingStatus; set => _bookingStatus = value; }
+        public int BookingId { get => _bookingId; set => _bookingId = value; }
+        public string? CustomerName { get => _customerName; set => _customerName = value; }
+    }
 }
