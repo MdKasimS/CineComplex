68b6ee3acf8ad960abff14ce3a11d76c7a0e4eba
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-02-01 13:41:15 +0530

    Show.cs is addes with getters and setters.

---
+++ OrderBooking/Show.cs
@@ -0,0 +1,23 @@
+namespace OrderBooking
+{
+    public class Show
+    {
+        private int _showId;
+        private int _movieId;
+        private int _theatreId;
+        private decimal _platinumSeatRate;
+        private decimal _goldSeatRate;
+        private decimal _silverSeatRate;
+        private DateTime _endDate;
+        private DateTime _startDate;
+
+        public int ShowId { get => _showId; set => _showId = value; }
+        public int MovieId { get => _movieId; set => _movieId = value; }
+        public int TheatreId { get => _theatreId; set => _theatreId = value; }
+        public decimal PlatinumSeatRate { get => _platinumSeatRate; set => _platinumSeatRate = value; }
+        public decimal GoldSeatRate { get => _goldSeatRate; set => _goldSeatRate = value; }
+        public decimal SilverSeatRate { get => _silverSeatRate; set => _silverSeatRate = value; }
+        public DateTime EndDate { get => _endDate; set => _endDate = value; }
+        public DateTime StartDate { get => _startDate; set => _startDate = value; }
+    }
+}
