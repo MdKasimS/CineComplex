1091492cea6d643cafd39a703be997c9dd4b2c69
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-02-01 13:03:47 +0530

    Finalised Booking.cs

--- OrderBooking/Booking.cs
+++ OrderBooking/Booking.cs
@@ -14,23 +14,14 @@ namespace OrderBooking
         private decimal _amount;
 
         public string? Email { get => _email; set => _email = value; }
-        public string? BookingStatus { get => BookingStatus1; set => BookingStatus1 = value; }
-        public int ShowId { get => ShowId1; set => ShowId1 = value; }
-
-        public List<int>? GetSeatNumbers1()
-        { return _seatNumbers; }
-        public void SetSeatNumbers1(List<int>? value)
-        {
-            _seatNumbers = value;
-        }
-
+        public int ShowId { get => ShowId; set => ShowId = value; }
         public string? SeatType { get => _seatType; set => _seatType = value; }
         public DateTime DateTime { get => _dateTime; set => _dateTime = value; }
         public decimal Amount { get => _amount; set => _amount = value; }
-        public int ShowId1 { get => _showId; set => _showId = value; }
         public int NumberOfSeats { get => _numberOfSeats; set => _numberOfSeats = value; }
-        public string? BookingStatus1 { get => _bookingStatus; set => _bookingStatus = value; }
         public int BookingId { get => _bookingId; set => _bookingId = value; }
         public string? CustomerName { get => _customerName; set => _customerName = value; }
+        public string? BookingStatus { get => _bookingStatus; set => _bookingStatus = value; }
+        public List<int>? SeatNumbers { get => _seatNumbers; set => _seatNumbers = value; }
     }
 }
