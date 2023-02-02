a497a90cf23168e23623e9ff0f7910d6a14f41b5
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-02-02 11:19:35 +0530

    Assets - CSV files, are added in repo.

--- OrderBooking/Booking.cs
+++ OrderBooking/Booking.cs
@@ -10,19 +10,19 @@ namespace OrderBooking
         private int _showId;
         private int _numberOfSeats;
         private List<int>? _seatNumbers;
-        private DateTime _dateTime;
+        private DateTime _bookingDate;
         private decimal _amount;
 
         public string? Email { get => _email; set => _email = value; }
         public int ShowId { get => _showId; set => _showId = value; }
         public string? SeatType { get => _seatType; set => _seatType = value; }
-        public DateTime DateTime { get => _dateTime; set => _dateTime = value; }
         public decimal Amount { get => _amount; set => _amount = value; }
         public int NumberOfSeats { get => _numberOfSeats; set => _numberOfSeats = value; }
         public int BookingId { get => _bookingId; set => _bookingId = value; }
         public string? CustomerName { get => _customerName; set => _customerName = value; }
         public string? BookingStatus { get => _bookingStatus; set => _bookingStatus = value; }
         public List<int>? SeatNumbers { get => _seatNumbers; set => _seatNumbers = value; }
+        public DateTime BookingDate { get => _bookingDate; set => _bookingDate = value; }
 
         public Booking(int sid, string cn, int nos, string seatType, string email, decimal amountToPay){
             ShowId = sid;
