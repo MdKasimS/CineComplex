4232822b5da1b1345b0cd2f5c349a2257ac52d28
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-02-03 19:53:07 +0530

    ZFrom this point project should start.

--- Program.cs
+++ Program.cs
@@ -12,5 +12,32 @@ internal class Program
         Console.WriteLine("\nChoose User Type : ");
         Console.WriteLine("\t1. Customer");
         Console.WriteLine("\t2. Admin");
+        Console.Write("Press 1 or 2 : ");
+
+        string ?userTypeInput = Console.ReadLine();
+        Console.WriteLine(userTypeInput);
+        Console.Clear();
+
+        if(userTypeInput != null){
+            
+        do{
+            choice = int.Parse(userTypeInput);
+            switch(choice){
+                case 1: 
+                    Console.Write("Enter LoginID : ");
+                    break;
+                case 2:
+                    Console.Write("Enter LoginID : ");
+                    break;
+                default:
+                    Console.WriteLine("Please enter the valid choice .....");
+                    break;
+            }
+        }while(choice>0 || choice<3);
+        }
+
+
+
+
     }
 }
