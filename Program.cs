d81241b603a9ea95eb19cd8031331b68fe5e6fe3
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-02-03 15:46:24 +0530

    Business logic started.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,16 @@
 ﻿using CineComplex;
 using OrderBooking;
 
-Console.WriteLine("\t----- Salam Hindusthan !!! -----");
-Console.WriteLine("============================================");
-Console.Write("Theatres : 4                       Shows : 16\n");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\t----- Salam Hindusthan !!! -----");
+        Console.WriteLine("================================================");
+        // Console.Write("Theatres : 4                         Shows : 16\n");
 
-Movie movie = new Movie("alhamdulillah" ,"kasim", "sache", "is", 8976.67, "great", "Mashallah" );
-
-movie.DisplayMovieDetails();
-
-Theatre theatre = new Theatre("PVRT-1", 500);
-theatre.DisplayTheaterDetails();
-
-Customer customer = new Customer("Vipul Bajaj", "Pune");
-customer.DisplayCustomerDetails();
-
-Booking book = new Booking(3234, "Vipul Bajaj", 4, "Platinum", "vb66@gmail.com", 459.65m);
-
-DateTime sdt = new DateTime(2023, 02, 01, 10, 30, 00);
-DateTime edt = new DateTime(2023, 02, 01, 13, 40, 00);
-Show show = new Show(456, 112, sdt, edt, 110.25m, 90.00m, 80.50m);
-show.DisplayShowDetails();
+        Console.WriteLine("\nChoose User Type : ");
+        Console.WriteLine("\t1. Customer");
+        Console.WriteLine("\t2. Admin");
+    }
+}
