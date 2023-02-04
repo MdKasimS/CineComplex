d9d50abcc17f4c03d714712520ee4d10c0378b8a
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-02-04 10:35:09 +0530

    Remote session was killed using Remote kill.

--- Program.cs
+++ Program.cs
@@ -12,14 +12,13 @@ internal class Program
         Console.WriteLine("\nChoose User Type : ");
         Console.WriteLine("\t1. Customer");
         Console.WriteLine("\t2. Admin");
+        Console.WriteLine("\t3. Exit");
         Console.Write("Press 1 or 2 : ");
 
         string? userTypeInput = "";
         int choice = 0;
-        string ?pwd = new String("");
-        string ?loginId = new String("");
-
-        
+        string? pwd = new String("");
+        string? loginId = new String("");
 
         do
         {
@@ -28,26 +27,35 @@ internal class Program
             {
                 choice = int.Parse(userTypeInput);
             }
-        switch (choice)
-        {
-            case 1:
-                Console.Clear();
-                Console.Write("Enter LoginID : ");
-                loginId = Console.ReadLine();
-                Console.Write("Enter Password : ");
-                pwd = Console.ReadLine();
-                System.Environment.Exit(0);
-                break;
-            case 2:
-                Console.Clear();
-                Console.Write("Enter LoginID : ");
-                System.Environment.Exit(0);
-                break;
-            default:
-                Console.WriteLine("Please enter the valid choice .....");
-                break;
-        }
-        } while (choice > 0 || choice < 3);
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.Write("Enter LoginID : ");
+                    loginId = Console.ReadLine();
+                    Console.Write("Enter Password : ");
+                    pwd = Console.ReadLine();
+                    
+                    Console.ReadKey();
+                    Console.Clear();
+                    System.Environment.Exit(0);
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.Write("Enter LoginID : ");
+                    System.Environment.Exit(0);
+                    break;
+
+                case 3:
+                    System.Environment.Exit(0);
+                    break;
+                    
+                default:
+                    Console.WriteLine("Please enter the valid choice .....");
+                    break;
+            }
+        } while (choice > 0 || choice < 4);
     }
 
 }
