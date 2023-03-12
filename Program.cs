f79e7734fd00b0147733ab5a2949c5426b8a64f9
kasim-wxh <45384577+MdKasimS@users.noreply.github.com>
2023-03-12 11:24:39 +0530

    Make understand!

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var client = new MongoClient("mongodb://localhost:27017");
+        var client = new MongoClient("mongodb://localhost:27017");//hard coded API
         var database = client.GetDatabase("CineComplex");
         var collection = database.GetCollection<BsonDocument>("Customer");
         
@@ -18,6 +18,7 @@ internal class Program
         
         Application app = new Application();
         app.run();
+        
         var docs = collection.Find(new BsonDocument()).Limit(5).ToList();
         Console.WriteLine(client.ListDatabases().ToList());
     }
