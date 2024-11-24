45f803afe5d36f55b209237229e4d9fc6464d8f0
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2024-11-24 23:17:17 +0530

    EFCore usage started.  Cretated DB Context class

---
+++ Classes/SQL/CineComplexDb.cs
@@ -0,0 +1,23 @@
+﻿using CineComplex.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineComplex.Classes.SQL
+{
+    public class CineComplexDb : DbContext
+    {
+        public DbSet<User> Users
+        {
+            get; set;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        { 
+            optionsBuilder.UseSqlite("Data Source=users.db"); 
+        }
+    }
+}
