00b26ae2d4cb638538c8f5dee2b8b94bd46f7d73
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2024-11-24 23:17:59 +0530

    Registration form is created

---
+++ ViewModels/FormViewModels/RegistrationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CineComplex.Classes.Base;
+using CineComplex.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineComplex.ViewModels.FormViewModels
+{
+    public class RegistrationViewModel:AViewModelBase<RegistrationViewModel>, IViewModel
+    {
+        public string
+    }
+}
