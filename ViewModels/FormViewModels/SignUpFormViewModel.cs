9fa87f67bbb390cb6b84160c6e412c3219f1a892
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2024-11-30 18:18:35 +0530

    save

--- ViewModels/FormViewModels/SignUpFormViewModel.cs
+++ ViewModels/FormViewModels/SignUpFormViewModel.cs
@@ -30,7 +30,7 @@ namespace CineComplex.ViewModels.FormViewModels
             _newUser.Password = Password;
             _newUser.Email = Email;
             _newUser.Contact = Contact;
-            if (await AuthenticationService.IsValidRegistration(_newUser))
+            if (await AuthenticationService.IsValidUserRegistration(_newUser))
             {
                 await User.CreateNewUser(_newUser);
 
