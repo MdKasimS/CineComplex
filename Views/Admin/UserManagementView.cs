7c92464a79d7bca7cd297301380ffd3bf1c0cb62
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2024-12-06 23:29:27 +0530

    UserManagementView added

---
+++ Views/Admin/UserManagementView.cs
@@ -0,0 +1,26 @@
+﻿using CineComplex.Classes.Base;
+using CineComplex.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineComplex.Views.Admin
+{
+    public class UserManagementView : AViewBase<UserManagementView>, IView
+    {
+        public int Choice { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<string> MenuList { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void LoadMenuList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
