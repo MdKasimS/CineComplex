d2ebc7829d30e5ef55d49e2c386b8cdfa2c628c4
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2024-12-15 03:25:28 +0530

    Handle Credential data in Vm as per MVVM principle

--- ViewModels/SignInViewModel.cs
+++ ViewModels/SignInViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CineComplex.Models;
 using CineComplex.Classes.Base;
+using CineComplex.Services;
 
 
 namespace CineComplex.ViewModels
@@ -22,7 +23,32 @@ namespace CineComplex.ViewModels
 
         public void ForgotPassword()
         {
+            //Now this method is in VM and we need View to be created when this method is called. Don't know how to handle it?
+            if(Instance.AreAllCredentialsAvailable())
+            {
 
+            }
+        }
+
+        private bool AreAllCredentialsAvailable()
+        {
+            if (string.IsNullOrWhiteSpace(Credential.Instance.LoginId)
+               || string.IsNullOrWhiteSpace(Credential.Instance.Password))
+            {
+                Console.Clear();
+                Console.WriteLine("All fields are required. Press any key to continue...");
+                Console.ReadKey();
+                return false;
+            }
+
+            if (!AuthenticationService.IsValidEmail(Credential.Instance.LoginId))
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid user Id format. Press any key to continue...");
+                Console.ReadKey();
+                return false;
+            }
+            return true;
         }
     }
 }
