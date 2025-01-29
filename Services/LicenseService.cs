3cae894671faff29d4935dc10c86a5f0dc99ac69
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-29 21:05:50 +0530

    For License service, LicenseService added

---
+++ Services/LicenseService.cs
@@ -0,0 +1,13 @@
+﻿using CineComplex.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineComplex.Services
+{
+    public class LicenseService: IService
+    {
+    }
+}
