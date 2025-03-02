63ae9e0ac6984efb3354120447eb6998148f1d41
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-03-02 21:28:25 +0530

    Added Ticket model

---
+++ Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineComplex.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public Show ShowBooked { get; set; }
+        public int ShowId { get; set; }
+        public Seat SeatBooked { get; set; }
+        public int SeatId { get; set; }
+
+    }
+}
