56605a142614524ded7bc61c4a6355d6692b82d5
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-03-02 21:37:21 +0530

    Address model updated and db migrated

---
+++ Migrations/20250302160651_UpdateAddress.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineComplex.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuildingDetails",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherDetails",
+                table: "Addresses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuildingDetails",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "OtherDetails",
+                table: "Addresses");
+        }
+    }
+}
--- Models/Address.cs
+++ Models/Address.cs
@@ -9,11 +9,14 @@ namespace CineComplex.Models
     public class Address
     {
         public int Id { get; set; }
+        public string BuildingDetails { get; set; }
         public string Area { get; set; }
         public string StreetName { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string Country { get; set; }
         public string PinCode { get; set; }
+        public string OtherDetails { get; set; }
+
     }
 }
