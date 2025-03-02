a44949a6e0bbada0daa2684b09135fd35fdf904e
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-03-02 22:01:29 +0530

    Adjustment of namesapces needed

---
+++ ViewModels/AdminViewModels/FormsViewModels/RegisterCineplexFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CineComplex.Classes.Base;
+using CineComplex.Interfaces;
+using CineComplex.ViewModels.FormViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineComplex.ViewModels.AdminViewModels.FormsViewModels
+{
+    public class RegisterCineplexFormViewModel : AViewModelBase<RegisterCineplexFormViewModel>, IViewModel
+    {
+        #region Properties
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        #endregion
+    }
+}
