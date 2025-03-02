8bdad34161d8bd9b2a94911706bcdbf55b5b569e
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-03-02 21:25:33 +0530

    udpate address model

--- Models/Address.cs
+++ Models/Address.cs
@@ -13,6 +13,7 @@ namespace CineComplex.Models
         public string StreetName { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string PinCode { get; set; }
     }
 }
