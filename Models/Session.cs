e5cac67cf2829265a350ea869bdf4dc76f2c099d
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-03-13 23:01:59 +0530

    Updated Sessions

--- Models/Session.cs
+++ Models/Session.cs
@@ -7,45 +7,46 @@ using System.Threading.Tasks;
 
 namespace CineComplex.Models
 {
-    public class Session 
-    { 
-        public int Id { get; set; } 
-        public string Token { get; set; } 
-        public DateTime LoginTimestamp { get; set; } 
-        public DateTime ExpirationTimestamp { get; set; } 
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public DateTime LoginTimestamp { get; set; }
+        public DateTime ExpirationTimestamp { get; set; }
         public User User { get; set; }
-        public int UserId { get; set; } 
+        public int UserId { get; set; }
 
         //TODO: Please make it in ORM EFCore standard way
 
-        public static async Task<int> CreateSessionAsync(Auth auth)
+        public static Session CreateSession(Auth auth)
         {
-            return await Task.Run(() =>
+
+            Session userSession = new Session()
             {
-                Session userSession = new Session()
-                {
-                    UserId = auth.UserId,
-                    Token = Guid.NewGuid().ToString(), // Generate a unique token
-                    LoginTimestamp = DateTime.Now,
-                    ExpirationTimestamp = DateTime.Now.AddHours(1)
-                };
+                UserId = auth.UserId,
+                Token = Guid.NewGuid().ToString(), // Generate a unique token
+                LoginTimestamp = DateTime.Now,
+                ExpirationTimestamp = DateTime.Now.AddHours(1)
+            };
 
-                SQLInteraction.Db.Sessions.Add(userSession);
-                SQLInteraction.Db.SaveChanges();
+            SQLInteraction.Db.Sessions.Add(userSession);
+            SQLInteraction.Db.SaveChangesAsync();
+            Credential.Instance.SessionTokenId = userSession.Token;
+            return userSession;
+        }
 
-                Credential.Instance.SessionTokenId = userSession.Token;
-
-                return userSession.Id;
-            });
-            
+        public static void CleanSessions()
+        {
+            SQLInteraction.Db.Sessions.RemoveRange(SQLInteraction.Db.Sessions);
+            SQLInteraction.Db.SaveChanges();
         }
 
         //This implements only one client can login or logout. Limiting screens
         //A new feature for multiple client with subscription can be developed
-        public async static Task DeleteSession(string tokenId)
+        public static void DeleteSession(string tokenId)
         {
-            Session session = SQLInteraction.Db.Sessions.FirstOrDefault(s => s.Token == tokenId); 
-            if (session != null) 
+            Session session = SQLInteraction.Db.Sessions.FirstOrDefault(s => s.Token == tokenId);
+            if (session != null)
             {
                 SQLInteraction.Db.Sessions.Remove(session);
                 SQLInteraction.Db.SaveChanges();
